15a4a645289f59d492154ae58e3096be7de3fb15
张文豪 <0009888@gmail.com>
2024-10-10 16:22:52 +0800

    增加触发多行补全的快捷键 #112465 【G-pilot】10月份开发IDEA和pycharm插件 https://ones.g-bits.com:21001/project/#/team/TYdk6vTk/task/6fvL9mbDStNE3KPQ

---
+++ src/main/kotlin/com/smallcloud/refactai/listeners/ForceMultiCompletionAction.kt
@@ -0,0 +1,39 @@
+package com.smallcloud.refactai.listeners
+
+
+import com.intellij.codeInsight.hint.HintManagerImpl.ActionToIgnore
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.editor.Caret
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.actionSystem.EditorAction
+import com.intellij.openapi.editor.actionSystem.EditorWriteActionHandler
+import com.smallcloud.refactai.Resources
+import com.smallcloud.refactai.modes.ModeProvider
+
+class ForceMultiCompletionAction :
+    EditorAction(InlineCompletionHandler()),
+    ActionToIgnore {
+    val ACTION_ID = "ForceMultiCompletionAction"
+
+    init {
+        this.templatePresentation.icon = Resources.Icons.LOGO_RED_16x16
+    }
+
+    class InlineCompletionHandler : EditorWriteActionHandler() {
+        override fun executeWriteAction(editor: Editor, caret: Caret?, dataContext: DataContext) {
+            Logger.getInstance("ForceMultiCompletionAction").debug("executeWriteAction")
+            val provider = ModeProvider.getOrCreateModeProvider(editor)
+            provider.beforeDocumentChangeNonBulk(null, editor)
+            provider.onTextChange(null, editor, true)
+        }
+
+        override fun isEnabledForCaret(
+            editor: Editor,
+            caret: Caret,
+            dataContext: DataContext
+        ): Boolean {
+            return true //InferenceGlobalContext.useForceCompletion
+        }
+    }
+}
